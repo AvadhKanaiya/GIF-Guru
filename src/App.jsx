1b7d72ef17967cf38bb2de27a2249161ed9972e7
Avadh Kanaiya <avadhkanaiya@gmail.com>
2024-06-28 15:25:55 +0530

    added analytics in gifguru

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Random from "./components/Random";
 import Tag from "./components/Tag";
+import { Analytics } from "@vercel/analytics/react"
+
 export default function App() {
   return (
     <div className="w-full h-screen flex flex-col background bg-cover relative items-center overflow-x-hidden">
@@ -7,6 +9,7 @@ export default function App() {
       <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
         <Random />
         <Tag />
+        <Analytics />
       </div>
     </div>
   );
